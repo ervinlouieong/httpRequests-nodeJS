eca53401df6a55416d88e8c85343b30b245bea53
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-08 19:59:08 +0000

    Initial commit

---
+++ getHTML.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      var dataString = data.toString();
+      callback(dataString);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions,printHTML);
