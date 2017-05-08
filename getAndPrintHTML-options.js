92796eced18ab9b3bdc112b8c6c64a5218d28e33
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-08 19:15:18 +0000

    Initial commit

---
+++ getAndPrintHTML-options.js
@@ -0,0 +1,28 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+  options = requestOptions;
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      var dataString = data.toString();
+      console.log(dataString);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML();
